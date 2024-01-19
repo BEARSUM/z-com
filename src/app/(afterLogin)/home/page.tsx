5bbea8548a47fe50f30137f2d4b3d9ce246f9023
[SW5] 문수민 <sum95771@gmail.com>
2024-01-20 05:36:08 +0900

    feat: PostForm 퍼블리싱 및 페이지에 추가

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,7 +1,7 @@
 import style from "./home.module.css";
 import Tab from "@/app/(afterLogin)/home/_component/Tab";
 import TabProvider from "@/app/(afterLogin)/home/_component/TabProvider";
-// import PostForm from "@/app/(afterLogin)/home/_component/PostForm";
+import PostForm from "@/app/(afterLogin)/home/_component/PostForm";
 // import Post from "@/app/(afterLogin)/_component/Post";
 
 export default function Home() {
@@ -9,8 +9,8 @@ export default function Home() {
     <main className={style.main}>
       <TabProvider>
         <Tab />
-        {/* <PostForm />
-        <Post/>
+        <PostForm />
+        {/* <Post/>
         <Post/>
         <Post/>
         <Post/>
