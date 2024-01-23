bf4b2acdd6590d712bed99570726477911766562
[SW5] 문수민 <sum95771@gmail.com>
2024-01-23 19:56:24 +0900

    feat: msw 2버전 변경사항 적용(client 여부 조건 추가)

--- src/app/_component/MSWComponent.tsx
+++ src/app/_component/MSWComponent.tsx
@@ -3,8 +3,7 @@ import { useEffect } from "react";
 
 export const MSWComponent = () => {
   useEffect(() => {
-    if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
-      require("@/mocks/brower");
+    if (typeof window !== "undefined") {
     }
   }, []);
 
